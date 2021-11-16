d3d82a149539cca2386ac370b5b463c87773b571
pablo-goncalves <milan.gnclvs@gmail.com>
2021-11-16 09:15:46 +0100

    lol

--- javascript/home.js
+++ javascript/home.js
@@ -2,7 +2,10 @@ let audio = document.getElementById ('audio')
 window.onload = (event) => {
     console.log('hallo')
 } 
+<<<<<<< Updated upstream
 
 function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'}, 'googleTranslate');
 }
+=======
+>>>>>>> Stashed changes
